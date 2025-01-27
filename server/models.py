d16396ff5ee3b7abc00a14c87d59fe726a67f698
Eric Richard <reric0802@gmail.com>
2025-01-27 21:45:35 +0300

    addded code to ensure passing

--- server/models.py
+++ server/models.py
@@ -17,8 +17,10 @@ metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
 
 
-class Zookeeper(db.Model):
+class Zookeeper(db.Model, SerializerMixin):
     __tablename__ = 'zookeepers'
+
+    serialize_rules = ('-animals.zookeeper',)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
@@ -26,9 +28,12 @@ class Zookeeper(db.Model):
 
     animals = db.relationship('Animal', back_populates='zookeeper')
 
+    
 
-class Enclosure(db.Model):
+class Enclosure(db.Model , SerializerMixin):
     __tablename__ = 'enclosures'
+
+    serialize_rules = ('-animals.enclosure',)
 
     id = db.Column(db.Integer, primary_key=True)
     environment = db.Column(db.String)
@@ -36,9 +41,14 @@ class Enclosure(db.Model):
 
     animals = db.relationship('Animal', back_populates='enclosure')
 
+    
 
-class Animal(db.Model):
+
+class Animal(db.Model, SerializerMixin):
     __tablename__ = 'animals'
+
+    serialize_rules = ('-zookeeper.animals', '-enclosure.animals')
+
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
@@ -50,5 +60,6 @@ class Animal(db.Model):
     enclosure = db.relationship('Enclosure', back_populates='animals')
     zookeeper = db.relationship('Zookeeper', back_populates='animals')
 
+    
     def __repr__(self):
         return f'<Animal {self.name}, a {self.species}>'
